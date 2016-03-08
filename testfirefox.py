0e516d5ffddbe77954ab2d71c260a70d9ed8cc17
jiangming1 <13858101782@qq.com>
2016-03-09 00:00:12 +0800

    a

---
+++ testfirefox.py
@@ -0,0 +1,34 @@
+# coding: UTF-8
+#作者 蒋明
+#作用 定点执行指定的web脚本 模拟人的操作去执行一系列动作
+#日期 2016-03-09
+from selenium import webdriver
+import time
+runtag='fj052_mag_prod_build_20160308.1'
+a='2016-3-9 00:30:00'
+a_struck_time=time.strptime(a,'%Y-%m-%d %H:%M:%S')
+print time.mktime(a_struck_time)
+print time.time()
+runtime=time.mktime(a_struck_time)
+while runtime>time.time():
+    time.sleep(6)
+    print "wait"
+driver=webdriver.Firefox()
+driver.get("http://192.168.180.18:8080/login?from=/")
+driver.find_element_by_id("j_username").clear()
+driver.find_element_by_id("j_username").send_keys("jiangming")
+driver.find_element_by_name("j_password").send_keys("kjt@123")
+driver.find_element_by_id("yui-gen1-button").click()
+time.sleep(3)
+driver.get("http://192.168.180.18:8080/view/%E7%94%9F%E4%BA%A7%E7%8E%AF%E5%A2%83/job/fj052_mag_prod_deploy/")
+driver.find_element_by_class_name("setting-input").send_keys(runtag)
+#driver.find_element_by_id("yui-gen1-button").click()
+time.sleep(3)
+driver.get("http://192.168.180.18:8080/view/%E7%81%BE%E5%A4%87%E7%8E%AF%E5%A2%83/job/fj052_mag_prod_deploy/")
+driver.find_element_by_class_name("setting-input").send_keys(runtag)
+#driver.find_element_by_id("yui-gen1-button").click()
+time.sleep(3)
+#yui-gen1-button
+print driver.title
+time.sleep(5)
+#driver.quit()
